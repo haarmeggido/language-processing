15c556d3c651ff3dbd5fe99e126f57940cea9885
Miłosz Góralczyk <goralczyk.milosz2k@gmail.com>
2023-10-22 18:46:00 +0200

    next

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
  
-#text = "De Tweede Wereldoorlog was de escalatie van de Tweede Chinees-Japanse Oorlog die begon in 1937 en een Europese oorlog begonnen in 1939 tot een militair conflict dat van 1941 tot 1945 op wereldschaal werd uitgevochten tussen twee allianties: de asmogendheden en de geallieerden. In het westen worden meestal de jaren 1939 en 1945 aangehouden als begin en eind van de oorlog."
+text = "De Tweede Wereldoorlog was de escalatie van de Tweede Chinees-Japanse Oorlog die begon in 1937 en een Europese oorlog begonnen in 1939 tot een militair conflict dat van 1941 tot 1945 op wereldschaal werd uitgevochten tussen twee allianties: de asmogendheden en de geallieerden. In het westen worden meestal de jaren 1939 en 1945 aangehouden als begin en eind van de oorlog."
  
 
-with open('ww2.txt', 'r') as file:
-  text = file.read().decode('utf-16-le')
+#with open('ww2.txt', 'r') as file:
+#  text = file.read().decode('utf-16-le')
 
 text = text.lower()
 l = "qwertyuiopasdfghjklzxcvbnm -áéíóúàèëïöü"
